a33a845e9d96aa0d83da7d1dd301cde327a869cc
Asim Balrabe Yazid <118350961+yazid27695@users.noreply.github.com>
2023-01-04 15:06:38 +0100

    adding more styling

--- app.js
+++ app.js
@@ -21,17 +21,17 @@ app.use(express.static('public'))
 app.get('/',(req,res)=>{
 	//sendfile is for loading normal html pages
 	//res.sendFile('./views/index.html',{root:__dirname})
-	res.render('index')
+	res.render('index',{title:'Home Page'})
 });
 
 app.get('/about',(req,res)=>{
 	//res.sendFile('./views/about.html',{root:__dirname})
-	res.render('about')
+	res.render('about',{title:'About us'})
 });
 
 app.get('/blogs/create',(req,res)=>{
 	//res.sendFile('./views/about.html',{root:__dirname})
-	res.render('create')
+	res.render('create',{title:'Registration Page'})
 });
 
 //redirect
@@ -43,7 +43,7 @@ app.get('/blogs/create',(req,res)=>{
 //404 middleware / it is synchoronous
 //Position matters alot
 app.use((req,res)=>{
-	res.status(404).render('404')
+	res.status(404).render('404',{title:'404 Page'})
 	//res.status(404).sendFile('./views/404.html',{root:__dirname})
 });
 
