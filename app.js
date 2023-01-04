85fbe9e13fd7a812fc3321110133e5f6834c4246
Asim Balrabe Yazid <118350961+yazid27695@users.noreply.github.com>
2023-01-04 14:43:40 +0100

    rendering html pages with ejs

--- app.js
+++ app.js
@@ -1,27 +1,49 @@
 const express = require('express')
+const morgan = require('morgan')
 
 //express app
 const app = express();
 
+//register view engine
+app.set('view engine','ejs')
+
+
+
 //listen for request
 app.listen(3000)
 
+//  logger middleware
+app.use(morgan('tiny')); //'tiny' or 'dev'
+
+//middleware and static files
+app.use(express.static('public'))
+
 app.get('/',(req,res)=>{
-	res.sendFile('./views/index.html',{root:__dirname})
+	//sendfile is for loading normal html pages
+	//res.sendFile('./views/index.html',{root:__dirname})
+	res.render('index')
 });
 
 app.get('/about',(req,res)=>{
-	res.sendFile('./views/about.html',{root:__dirname})
+	//res.sendFile('./views/about.html',{root:__dirname})
+	res.render('about')
 });
 
+app.get('/blogs/create',(req,res)=>{
+	//res.sendFile('./views/about.html',{root:__dirname})
+	res.render('create')
+});
 
 //redirect
-app.get('/about-me',(req,res)=>{
-	res.redirect('/about')
-});
+// app.get('/about-me',(req,res)=>{
+// 	res.redirect('/about')
+// });
+
 
 //404 middleware / it is synchoronous
 //Position matters alot
 app.use((req,res)=>{
-	res.status(404).sendFile('./views/404.html',{root:__dirname})
+	res.status(404).render('404')
+	//res.status(404).sendFile('./views/404.html',{root:__dirname})
 });
+
--- script.js
+++
@@ -1,6 +0,0 @@
-const c = require('./app2.js')
-
-let x = 10;
-let y = c.largeNumber
-
-console.log(x+y)
